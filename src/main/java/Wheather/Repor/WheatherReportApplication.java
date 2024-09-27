d3c122bac6465ed793896faefca02c58202cf059
a1 <a1123@gmail.com>
2024-09-27 11:36:03 +0530

    first commit

---
+++ src/main/java/Wheather/Repor/WheatherReportApplication.java
@@ -0,0 +1,17 @@
+package Wheather.Repor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheatherReportApplication 
+{
+
+	public static void main(String[] args) 
+	{
+	   SpringApplication.run(WheatherReportApplication.class, args);
+	}
+ 
+
+}
+ 
---
+++ src/test/java/Wheather/Repor/WheatherReportApplicationTests.java
@@ -0,0 +1,13 @@
+package Wheather.Repor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheatherReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
